5bd3c4f6601d90f9a055c3bfb552b29c7e84eedd
Md Mahmudul Hasan <51690967+hasanmdmahmudul@users.noreply.github.com>
2021-10-17 00:00:49 +1100

    Create Gamescene1.js

---
+++ Gamescene1.js
@@ -0,0 +1,13 @@
+mouseClicked = function() {
+    // Scene 2
+    background(173, 239, 255);
+    fill(7, 14, 145);
+    text("Lil Winston is born!", 10, 100);
+    image(getImage("creatures/BabyWinston"), width/2, height/2);
+};
+
+// Scene 1
+background(235, 247, 255);
+fill(0, 85, 255);
+textSize(39);
+text("The Story of Winston", 10, height/2);
